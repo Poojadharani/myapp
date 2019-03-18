bb557576ee7ee7a8e0427fba76187df017cee9b9
Dharani <lakshmim.velsol@gmail.com>
2019-03-18 16:27:37 +0530

    usertype changes

--- src/app/addusertype/addusertype.component.ts
+++ src/app/addusertype/addusertype.component.ts
@@ -19,9 +19,9 @@ modelu:Usertype[];
   status:string;
   public rows: Array<{sno: string, code: string, name: string}> = [];
   onclick(){
-     console.log(this.sno);
-     console.log(this.code);
-     console.log(this.name);
+     //console.log(this.sno);
+     //console.log(this.code);
+     //console.log(this.name);
     this.rows.push( {sno: this.sno, code: this.code, name: this.name } );
     this.status="Added Successfully";
     this.sno='';this.code='';this.name='';
